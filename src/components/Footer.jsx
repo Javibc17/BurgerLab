e2d855ff9dffabebef0b6a6618a870688190ec51
Javier <javibece1717@gmail.com>
2025-05-12 12:35:19 +0200

    Improved Menu

--- src/components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-function Footer() {
-  return (
-    <footer className="footer">
-      <p>&copy; 2025 BurgerLab. Todos los derechos reservados.</p>
-    </footer>
-  );
-}
-
-export default Footer;
